810acdd2fee1211729e7b0b8d30509e6cb6c5d5c
ie04 <iyadeltifi12@gmail.com>
2026-06-12 19:07:55 -0400

    Apply Jost globally across storefront

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,8 +37,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${jost.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+    <html lang="en" className={`${jost.variable} ${jost.className} h-full antialiased`}>
+      <body
+        className={`${jost.className} min-h-full flex flex-col`}
+        suppressHydrationWarning
+      >
         <Providers>{children}</Providers>
       </body>
     </html>
